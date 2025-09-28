93cebe5fdc73d6133452577635ecc0290087481d
Mareedu Saibabu <a3097640@bestbuy.com>
2025-09-28 23:31:31 +0530

    User Service

---
+++ micros/user-service/src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const PORT = Number(process.env?.["USER_SERVICE_PORT"]) || 3001;
+const HOST = process.env?.["USER_SERVICE_HOST"] || "localhost";
+
+// Start the User Service
+app.listen(PORT, HOST, () => {
+  console.log(`🚀🚀 Users service running on http://${HOST}:${PORT}`);
+});
