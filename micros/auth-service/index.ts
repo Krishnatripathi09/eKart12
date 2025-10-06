117526566acb88f2be5b365abce03c898952bfd6
Krishnatripathi09 <192.168.9.99kj@gmail.com>
2025-10-06 11:49:23 +0530

    feat(user):Created user model and synced with database

--- micros/auth-service/index.ts
+++ micros/auth-service/index.ts
@@ -6,7 +6,3 @@ const PORT = 4001;
 app.listen(PORT, () => {
   console.log(`Server is Listening on ${PORT}`);
 });
-
-app.get('/', (req, res) => {
-  res.status(200).send('This is a Get Route');
-});
